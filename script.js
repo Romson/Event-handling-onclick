16d89b892deb71ff6c3058a9581eeae8ff693dee
Romero Lijkwan <romerolijkwan@gmail.com>
2018-06-08 18:16:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ function handleClick() {
 }
 
 // Second reference 
-document.getElementById('dateButton').onclick = handleClick;Event handling onclick date
+document.getElementById('dateButton').onclick = handleClick;
 function handleClick() {
     document.getElementById('dateTarget').innerHTML = Date();
-} 
+}
 
 // Named identifier
 document.getElementById('dateButton').addEventListener('click', handleClick);
 function handleClick() {
     document.getElementById('dateTarget').innerHTML = Date();
-} 
+}
 
 // Anonymous function
 document.getElementById('dateButton').addEventListener('click', function handleClick() {
